50bd67e767a81dfdb630abf255f89777b14fb964
b04705030 <b04705030@g.ntu.edu.tw>
2021-04-29 02:13:29 +0800

    Embed youtube video player

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+import Navbar from './components/Navbar';
+
+const VIDEO_IDs = {
+  'lofi-girl': '5qap5aO4i9A',
+  'tokyo-cafe': '6uddGul0oAc'
+};
+
+const useStyles = makeStyles((theme) => ({
+  app: {
+    height: '100vh',
+    backgroundColor: 'black'
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+    textAlign: 'center'
+  },
+  appBarSpacer: theme.mixins.toolbar,
+  videoPlayer: {
+    width: 800,
+    height: 450,
+    pointerEvents: 'none'
+  }
+}));
 
 function App() {
+  const classes = useStyles();
+  const [videoId, setVideoId] = useState(VIDEO_IDs['lofi-girl']);
+  let iframeSrc = `https://www.youtube.com/embed/${videoId}?controls=0&autoplay=1&disablekb=1&modestbranding=1&rel=0`;
+
+  const onChannelClicked = (name) => {
+    setVideoId(VIDEO_IDs[name]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.app}>
+      <Navbar onChannelClicked={onChannelClicked}/>
+      <Container maxWidth="md" className={classes.container}>
+        <div className={classes.appBarSpacer} />
+        <iframe className={classes.videoPlayer} src={iframeSrc} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      </Container>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,45 @@
+import React from "react";
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+// import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+
+// import { FormattedMessage } from 'react-intl';
+// import { LOCALE_OPTIONS } from '../i18n/locale-settings';
+const useStyles = makeStyles((theme) => ({
+    appBar: {
+        backgroundColor: 'black',
+        zIndex: 1201
+    }
+}));
+
+function Navbar({ onChannelClicked }) {
+    const classes = useStyles();
+
+    const handleChannelClicked = (_, channelName) => {
+        onChannelClicked(channelName);
+    }
+
+    return (
+        <AppBar position="fixed" className={classes.appBar}>
+            <Toolbar>
+                <Button color="inherit">
+                    Mood for Study
+                </Button>
+                <Button color="inherit" onClick={(event) => handleChannelClicked(event, "tokyo-cafe")}>
+                    Tokyo Cafe
+                </Button>
+                <Button color="inherit" onClick={(event) => handleChannelClicked(event, "lofi-girl")}>
+                    Lofi Girl
+                </Button>
+                
+                {/* <div style={{ flexGrow: 1 }}></div> */}
+                {/* <Button color="inherit" onClick={() => dispatch(setLocale(LOCALE_OPTIONS.zh))}>中文</Button>
+                <Button color="inherit" onClick={() => dispatch(setLocale(LOCALE_OPTIONS.en))}>English</Button> */}
+            </Toolbar>
+        </AppBar>
+    );
+}
+
+export default Navbar;
